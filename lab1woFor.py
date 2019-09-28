52540594042885c276306b5ce95f35cb2e2f2f3f
emedvedev97 <55349733+emedvedev97@users.noreply.github.com>
2019-09-29 01:48:11 +0300

    Update lab1woFor.py

--- lab1woFor.py
+++ lab1woFor.py
@@ -5,16 +5,11 @@ from keras.datasets import mnist
 X_size = 5
 Y_size = 3
 
-#train_data = np.loadtxt("mnist_train.csv", delimiter=",")
-#train_imgs = np.asfarray(train_data[:, 1:])
-
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#temp = train_imgs.reshape(60000,28,28)
-temp = X_train.reshape(60000,28,28)
 nummas = np.random.randint(0, 60000, X_size*Y_size)
 
-vvv = temp[nummas]
+vvv = X_train[nummas]
 vvv = vvv.reshape(Y_size,X_size,28,28)
 vvv = vvv.transpose(0,2,1,3).reshape(Y_size*28,X_size*28)
 
